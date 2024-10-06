892a59a19b9091a6a9d0c4715c664337f76b0257
johncopro51 <johncopro51@gmail.com>
2024-10-06 17:41:54 -0500

    added docker scalability

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
+import socket
+from flask_redis import FlaskRedis
+
 
 app= Flask(__name__)
+redis_client = FlaskRedis(app)
 app.secret_key='lol'
 
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///salaries.db"
@@ -27,7 +31,7 @@ def home():
             db.session.add(new_user)
             db.session.commit()
             return redirect(url_for('home'))
-        return render_template('index.html')
+        return render_template('index.html',container=socket.gethostname())
 
 @app.route('/names')
 def names():
@@ -43,4 +47,4 @@ if __name__=='__main__':
     with app.app_context():
      db.create_all()
 
-    app.run(debug=True)
+    app.run(debug=True,host = '0.0.0.0',port=3000,)
